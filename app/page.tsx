55f4153a77c8d8f99294bca74a20c1a26c16b369
Kristen Chandler <krismchandler@gmail.com>
2024-08-12 10:51:55 -0500

    noted the homepage

--- app/page.tsx
+++ app/page.tsx
@@ -1,5 +1,7 @@
 import Image from "next/image";
 
+//this will be the home page
+
 export default function Home() {
   return (
     <main className="flex min-h-screen flex-col items-center justify-between p-24">
