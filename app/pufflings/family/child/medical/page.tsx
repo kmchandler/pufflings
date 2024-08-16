16b4bec1affe449c18064c6f78e75f38761eb94c
Kristen Chandler <krismchandler@gmail.com>
2024-08-16 10:22:04 -0500

    created pages for the individual child data items with navbar and placeholder text

---
+++ app/pufflings/family/child/medical/page.tsx
@@ -0,0 +1,14 @@
+import NavBar from "@/app/ui/navigation";
+
+const Medical = () => {
+  return (
+    <div className="flex flex-col">
+        <NavBar />
+      <div className="mt-36 self-center">
+        MEDICALPAGE text here
+      </div>
+    </div>
+  );
+};
+
+export default Medical;
---
+++ app/pufflings/family/child/sleeps/page.tsx
@@ -0,0 +1,14 @@
+import NavBar from "@/app/ui/navigation";
+
+const Sleeps = () => {
+  return (
+    <div className="flex flex-col">
+        <NavBar />
+      <div className="mt-36 self-center">
+        SLEEPS PAGE text here
+      </div>
+    </div>
+  );
+};
+
+export default Sleeps;
