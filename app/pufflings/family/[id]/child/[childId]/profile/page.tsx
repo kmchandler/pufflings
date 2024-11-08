186aeb2617e7aa0baba86018b8756f5006e1396b
Kristen Chandler <krismchandler@gmail.com>
2024-11-08 14:24:00 -0600

    age calculation working in years and months based on childs age

--- app/pufflings/family/[id]/child/[childId]/profile/page.tsx
+++ app/pufflings/family/[id]/child/[childId]/profile/page.tsx
@@ -5,6 +5,7 @@ import { lastCreatedWeight } from "@/lib/weight";
 import Link from "next/link";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { faPenToSquare } from '@fortawesome/free-solid-svg-icons'
+import { calculateAge, getMonthsDifference } from "@/lib/currentAge";
 
 
 const childProfilePage = async ({ params: { childId, id }}: {params: { childId: string, id:string }}) => {
@@ -19,18 +20,32 @@ const childProfilePage = async ({ params: { childId, id }}: {params: { childId: 
   const day = String(date?.getUTCDate()).padStart(2, '0');
   const year = String(date?.getUTCFullYear());
 
-  const formattedBirthday = `${month}/${day}/${year}`;
-  console.log(formattedBirthday, "formattedBday")
+  let formattedBirthday = `${month}/${day}/${year}`;
+
+  if (!date) {
+    formattedBirthday = ''
+  }
+
+  const currentAge = calculateAge(date);
+
+  let ageDisplay = ''
+
+  if (currentAge >= 1) {
+    let childsAge = currentAge
+    ageDisplay = `${childsAge} years`
+  } else if (currentAge < 1) {
+   let childsAge = getMonthsDifference(date);
+   ageDisplay = `${childsAge} months`
+  }
   
   const editIcon = <FontAwesomeIcon icon={faPenToSquare} />
-
-  // Info I'd like to have on this page- age(need to collect birthday to calculate), current height, current weight, (possibly blood type?)
 
   if (currentHeight && currentWeight) {
     return (
       <div className="flex flex-col">
       <div className="text-oxford-blue py-2 px-4 rounded shadow flex bg-tea-green transition hover:drop-shadow-xl transition-all transition-duration-100 text-3xl flex flex-col mt-4 inline-block text-center">
         <p className="mb-2 text-5xl">{childInfo?.name.toLocaleLowerCase()} {familyInfo.family_name.toLowerCase()}</p>
+        <p className="mb-2">age: {ageDisplay}</p>
         <p className="mb-2">birthday: {formattedBirthday}</p>
         <p className="flex flex-row self-center">height: {currentHeight?.feet} ft {currentHeight?.inches} in
           <Link href={`/pufflings/family/${id}/child/${childId}/profile/editHeight`} className="text-xl flex ml-3 mt-2">{editIcon}</Link>
@@ -48,6 +63,7 @@ const childProfilePage = async ({ params: { childId, id }}: {params: { childId: 
       <div className="flex flex-col">
       <div className="text-oxford-blue py-2 px-4 rounded shadow flex bg-tea-green transition hover:drop-shadow-xl transition-all transition-duration-100 text-3xl flex flex-col mt-4 inline-block text-center">
         <p className="mb-2 text-5xl">{childInfo?.name.toLocaleLowerCase()} {familyInfo.family_name.toLowerCase()}</p>
+        <p className="mb-2">age: {ageDisplay}</p>
         <p className="mb-2">birthday: {formattedBirthday}</p>
         <p className="flex flex-row self-center">height
           <Link href={`/pufflings/family/${id}/child/${childId}/profile/editHeight`} className="text-xl flex ml-3 mt-2">{editIcon}</Link>
@@ -65,6 +81,7 @@ const childProfilePage = async ({ params: { childId, id }}: {params: { childId: 
       <div className="flex flex-col">
       <div className="text-oxford-blue py-2 px-4 rounded shadow flex bg-tea-green transition hover:drop-shadow-xl transition-all transition-duration-100 text-3xl flex flex-col mt-4 inline-block text-center">
         <p className="mb-2 text-5xl">{childInfo?.name.toLocaleLowerCase()} {familyInfo.family_name.toLowerCase()}</p>
+        <p className="mb-2">age: {ageDisplay}</p>
         <p className="mb-2">birthday: {formattedBirthday}</p>
         <p className="flex flex-row self-center">height: {currentHeight?.feet} ft {currentHeight?.inches} in
           <Link href={`/pufflings/family/${id}/child/${childId}/profile/editHeight`} className="text-xl flex ml-3 mt-2">{editIcon}</Link>
@@ -82,6 +99,7 @@ const childProfilePage = async ({ params: { childId, id }}: {params: { childId: 
       <div className="flex flex-col">
       <div className="text-oxford-blue py-2 px-4 rounded shadow flex bg-tea-green transition hover:drop-shadow-xl transition-all transition-duration-100 text-3xl flex flex-col mt-4 inline-block text-center">
         <p className="mb-2 text-5xl">{childInfo?.name.toLocaleLowerCase()} {familyInfo.family_name.toLowerCase()}</p>
+        <p className="mb-2">age: {ageDisplay}</p>
         <p className="mb-2">birthday: {formattedBirthday}</p>
         <p className="flex flex-row self-center">height
           <Link href={`/pufflings/family/${id}/child/${childId}/profile/editHeight`} className="text-xl flex ml-3 mt-2">{editIcon}</Link>
