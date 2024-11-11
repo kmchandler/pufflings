faf321e0d14ad99bb4a6504526eb0368e0336042
Kristen Chandler <krismchandler@gmail.com>
2024-11-11 14:42:38 -0600

    confirming fixes merge

--- app/pufflings/addFamily.tsx
+++ app/pufflings/addFamily.tsx
@@ -4,9 +4,15 @@ const AddFamilyForm = () => {
 
   return (
     <form action={createFamily}>
-      hello world
-      <input type="text" name="family_name" />
-      <button type="submit"> submit family </button>
+      <div className="flex flex col">
+        create a new family below, or ask to be added to an existing family.
+      </div>
+      
+      <label        
+        className="text-3xl">family name
+      </label>
+      <input type="text" name="family_name" className="outline outline-1 outline-oxford-blue rounded text-2xl" />
+      <button type="submit" className="text-oxford-blue py-2 px-4 rounded shadow flex bg-tea-green transition hover:drop-shadow-xl transition-all transition-duration-100 text-xl mt-7"> submit new family </button>
     </form>
   )
 }
