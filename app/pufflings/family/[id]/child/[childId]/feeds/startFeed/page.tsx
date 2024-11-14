97c758a396e61b7aaa9adb062d42b694e200e4f2
Ryann Chandler <ryann.chandler@gmail.com>
2024-11-14 11:57:13 -0600

    add back feed butuon

--- app/pufflings/family/[id]/child/[childId]/feeds/startFeed/page.tsx
+++ app/pufflings/family/[id]/child/[childId]/feeds/startFeed/page.tsx
@@ -3,10 +3,10 @@ import { logFeedStart } from "@/lib/feed"
 const startFeed = ({ params: { childId }}: {params: { childId: string }}) => {
 
   return (
-    <form action={logFeedStart}>
-      <input name="childId" value={childId} hidden/>
-      <button type="submit" className="text-oxford-blue py-2 px-4 rounded shadow flex order-3 bg-tea-green transition hover:drop-shadow-xl transition-all transition-duration-100 text-xl flex flex-col mt-7">start feed</button>
-    </form>
+      <form action={logFeedStart} className="flex justify-center">
+        <input name="childId" value={childId} hidden/>
+        <button type="submit" className="text-oxford-blue py-2 px-4 rounded shadow flex order-3 bg-tea-green hover:drop-shadow-xl transition-all transition-duration-100 text-xl flex-col mt-7">start feed</button>
+      </form>
   )
 }
 
