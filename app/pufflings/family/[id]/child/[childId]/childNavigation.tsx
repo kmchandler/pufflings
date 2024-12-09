3ef9c70a0edf9321436e2c01d7e6161cb1d20c01
rchandler-eb <rchandler@eventbrite.com>
2024-12-09 08:48:04 -0600

    fixing type

--- app/pufflings/family/[id]/child/[childId]/childNavigation.tsx
+++ app/pufflings/family/[id]/child/[childId]/childNavigation.tsx
@@ -13,7 +13,7 @@ const tabs = [
   { name: 'Medical', href: 'medical', current: false },
 ]
 
-function classNames(...classes) {
+function classNames(...classes: any) {
   return classes.filter(Boolean).join(' ')
 }
 
