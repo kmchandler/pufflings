0f157aff6e4700af78005048db6b5279258ed8dc
Kristen Chandler <krismchandler@gmail.com>
2025-04-07 10:26:34 -0500

    working to create solid Feeds

--- app/pufflings/family/[id]/child/[childId]/feeds/[feedId]/solidsFeed/addSolids/page.tsx
+++ app/pufflings/family/[id]/child/[childId]/feeds/[feedId]/solidsFeed/addSolids/page.tsx
@@ -1,7 +1,8 @@
 'use client';
 
 import SubmitButton from '@/app/ui/submitButton';
-import { createBottle } from '@/lib/feed';
+import { createSolid } from '@/lib/feed';
+import { ChevronDownIcon } from '@heroicons/react/16/solid';
 
 const AddSolidsForm = ({
   params: { childId, feedId },
@@ -9,19 +10,90 @@ const AddSolidsForm = ({
   params: { childId: string; feedId: string };
 }) => {
   return (
-    <form action={createBottle} className='flex flex-col items-center'>
-      <label className='mb-2 self-center text-3xl'>amount</label>
-      <div className='flex flex-row'>
-        <input
-          name='amount'
-          className='relative mr-2 w-12 self-center rounded text-center text-xl text-oxford-blue outline outline-1 outline-oxford-blue'
-        />
-        <div className='text-xl'>oz</div>
-      </div>
+    <form action={createSolid} className='flex flex-col items-center'>
+      <div className='space-y-12 sm:space-y-16'>
+        <div>
+          <div className='sm:grid sm:grid-cols-3 sm:items-start sm:gap-4 sm:py-6'>
+            <label htmlFor='type' className='block'>
+              type
+            </label>
+            <div className='mt-2 sm:col-span-2 sm:mt-0'>
+              <div className='flex items-center rounded-md bg-white pl-3 outline-1 -outline-offset-1 outline-gray-300 focus-within:outline-2 focus-within:-outline-offset-2 focus-within:outline-indigo-600 sm:max-w-md'>
+                <div className='shrink-0 select-none text-base text-gray-500 sm:text-sm/6'></div>
+                <input
+                  id='type'
+                  name='type'
+                  type='text'
+                  placeholder='ex: puree'
+                  className='block min-w-0 grow py-1.5 pl-1 pr-3 text-base text-gray-900 placeholder:text-gray-400 focus:outline-none sm:text-sm/6'
+                />
+              </div>
+            </div>
+          </div>
 
-      <input name='childId' value={childId} hidden />
-      <input name='feedId' value={feedId} hidden />
-      <SubmitButton message='submit' />
+          <div className='mt-2 space-y-8 border-b border-gray-900/10 pb-12 sm:space-y-0 sm:divide-y sm:divide-gray-900/10 sm:border-t sm:pb-0'>
+            <div className='sm:grid sm:grid-cols-3 sm:items-start sm:gap-4 sm:py-6'>
+              <label htmlFor='type' className='block'>
+                amount
+              </label>
+              <div className='mt-2 sm:col-span-2 sm:mt-0'>
+                <div className='flex items-center rounded-md bg-white pl-3 outline-1 -outline-offset-1 outline-gray-300 focus-within:outline-2 focus-within:-outline-offset-2 focus-within:outline-indigo-600 sm:max-w-md'>
+                  <div className='shrink-0 select-none text-base text-gray-500 sm:text-sm/6'></div>
+                  <input
+                    id='amount'
+                    name='amount'
+                    type='text'
+                    placeholder='ex: half a jar'
+                    className='block min-w-0 grow py-1.5 pl-1 pr-3 text-base text-gray-900 placeholder:text-gray-400 focus:outline-none sm:text-sm/6'
+                  />
+                </div>
+              </div>
+            </div>
+          </div>
+
+          <div className='sm:grid sm:grid-cols-3 sm:items-start sm:gap-4 sm:py-6'>
+            <label htmlFor='type' className='block'>
+              flavor
+            </label>
+            <div className='mt-2 sm:col-span-2 sm:mt-0'>
+              <div className='flex items-center rounded-md bg-white pl-3 outline-1 -outline-offset-1 outline-gray-300 focus-within:outline-2 focus-within:-outline-offset-2 focus-within:outline-indigo-600 sm:max-w-md'>
+                <div className='shrink-0 select-none text-base text-gray-500 sm:text-sm/6'></div>
+                <input
+                  id='flavor'
+                  name='flavor'
+                  type='text'
+                  placeholder='ex: apple'
+                  className='block min-w-0 grow py-1.5 pl-1 pr-3 text-base text-gray-900 placeholder:text-gray-400 focus:outline-none sm:text-sm/6'
+                />
+              </div>
+            </div>
+          </div>
+
+          <div className='mt-2 space-y-8 border-b border-gray-900/10 pb-12 sm:space-y-0 sm:divide-y sm:divide-gray-900/10 sm:border-t sm:pb-0'>
+            <div className='sm:grid sm:grid-cols-3 sm:items-start sm:gap-4 sm:py-6'>
+              <label htmlFor='type' className='block'>
+                notes
+              </label>
+              <div className='mt-2 sm:col-span-2 sm:mt-0'>
+                <div className='flex items-center rounded-md bg-white pl-3 outline-1 -outline-offset-1 outline-gray-300 focus-within:outline-2 focus-within:-outline-offset-2 focus-within:outline-indigo-600 sm:max-w-md'>
+                  <div className='shrink-0 select-none text-base text-gray-500 sm:text-sm/6'></div>
+                  <input
+                    id='notes'
+                    name='notes'
+                    type='textarea'
+                    placeholder='ex: seemed to enjoy it'
+                    className='block min-w-0 grow py-1.5 pl-1 pr-3 text-base text-gray-900 placeholder:text-gray-400 focus:outline-none sm:text-sm/6'
+                  />
+                </div>
+              </div>
+            </div>
+          </div>
+
+          <input name='childId' value={childId} hidden />
+          <input name='feedId' value={feedId} hidden />
+          <SubmitButton message='submit' />
+        </div>
+      </div>
     </form>
   );
 };
--- lib/feed.ts
+++ lib/feed.ts
@@ -113,6 +113,122 @@ export const createBottle = async (input: FormData) => {
   redirect(`/pufflings/family/${family.id}/child/${childId}/feeds`);
 };
 
+export const logSolidStart = async (input: FormData) => {
+  const user = await currentUser();
+  if (!user) throw new Error('no user');
+
+  const familyUser = await prisma.family_user.findFirstOrThrow({
+    where: { user_id: user.id },
+    include: {
+      family: true,
+    },
+  });
+
+  const family = familyUser.family;
+
+  const inputChildId = input.get('childId');
+  if (!inputChildId) return;
+
+  const childId: number = Number(inputChildId);
+
+  const feed = await prisma.feed.create({
+    data: {
+      start_time: new Date(),
+      child_id: childId,
+      user_id: user.id,
+    },
+  });
+
+  redirect(
+    `/pufflings/family/${family.id}/child/${childId}/feeds/${feed.id}/solidsFeed/endSolids`
+  );
+};
+
+export const logSolidEnd = async (input: FormData) => {
+  const user = await currentUser();
+  if (!user) throw new Error('no user');
+
+  const familyUser = await prisma.family_user.findFirstOrThrow({
+    where: { user_id: user.id },
+    include: {
+      family: true,
+    },
+  });
+
+  const family = familyUser.family;
+
+  const inputChildId = input.get('childId');
+  if (!inputChildId) return;
+
+  const inputFeedId = input.get('feedId');
+  if (!inputFeedId) return;
+
+  const childId: number = Number(inputChildId);
+  const feedId: number = Number(inputFeedId);
+
+  const feed = await prisma.feed.update({
+    where: { id: feedId },
+    data: {
+      end_time: new Date(),
+    },
+  });
+
+  redirect(
+    `/pufflings/family/${family.id}/child/${childId}/feeds/${feed.id}/solidsFeed/addSolids`
+  );
+};
+
+export const createSolid = async (input: FormData) => {
+  const user = await currentUser();
+  if (!user) throw new Error('no user');
+
+  const familyUser = await prisma.family_user.findFirstOrThrow({
+    where: { user_id: user.id },
+    include: {
+      family: true,
+    },
+  });
+
+  const family = familyUser.family;
+
+  const inputChildId = input.get('childId');
+  if (!inputChildId) return;
+
+  const inputFeedId = input.get('feedId');
+  if (!inputFeedId) return;
+
+  const inputType = input.get('type');
+  if (!inputType) return;
+
+  const inputAmount = input.get('amount');
+  if (!inputAmount) return;
+
+  const inputFlavor = input.get('flavor');
+  if (!inputFlavor) return;
+
+  const inputNotes = input.get('notes');
+  if (!inputNotes) return;
+
+  const childId: number = Number(inputChildId);
+  const feedId: number = Number(inputFeedId);
+  const type: string = String(inputType);
+  const amount: number = Number(inputAmount);
+  const flavor: string = String(inputFlavor);
+  const notes: string = String(inputNotes);
+
+  const feed = await prisma.feed.update({
+    where: { id: feedId },
+    data: {
+      type: type,
+      amount: new Prisma.Decimal(amount),
+      flavor: flavor,
+      notes: notes,
+    },
+  });
+
+  redirect(`/pufflings/family/${family.id}/child/${childId}/feeds`);
+};
+
 export const lastCreatedFeed = async (childId: string) => {
   const user = await currentUser();
   if (!user) throw new Error('no user');
